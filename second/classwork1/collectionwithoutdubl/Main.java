ecf1d015d6037211551cdb0b0dcf9f604c3ac79f
shycoldii <57770001+shycoldii@users.noreply.github.com>
2020-10-04 19:32:29 +0300

    cl2
    Классная работа с методами по коллекциям в папке second

--- second/classwork1/collectionwithoutdubl/Main.java
+++ second/classwork1/collectionwithoutdubl/Main.java
@@ -1,5 +1,28 @@
 package second.classwork1.collectionwithoutdubl;
 
-public class Main {
+import java.util.ArrayList;
+import java.util.Collection;
 
+public class Main {
+     public static void main(String[] args){
+         ArrayList<Integer> intlist = new ArrayList<>();
+         ArrayList<Character> charlist =  new ArrayList<>();
+
+         ArrayList<Integer> new_array = new ArrayList<>(); //создание массива
+         for (int i =0;i<22; i++){
+             new_array.add(1);
+         }
+         // removeAll удаляет ВСЕ вхождения из переданной коллекции
+         //intlist.removeAll(new_array);
+         //System.out.println(intlist.size());
+         intlist.addAll(new_array);
+         System.out.println("Массив: "+new_array.toString());
+         Collection<Integer> my_c = collection.removeDuplicates(new_array);
+         System.out.println("Итоговый результат массива: "+my_c.toString());
+         //используем метод удаления
+
+         String my_string = "привет";
+         collection.mapping(my_string);
+         collection.buildDictionaryWithMap(my_string);
+     }
 }
---
+++ second/classwork1/collectionwithoutdubl/collection.java
@@ -0,0 +1,74 @@
+package second.classwork1.collectionwithoutdubl;
+import java.util.*;
+
+public class collection {
+    public static <T>Collection<T> removeDuplicates(Collection<T> coll){
+        /**
+         * Возвращает коллекцию без дубликатов
+         */
+        //T- любой тип данных
+        return new HashSet<>(coll);
+    }
+
+    public static  void mapping(String text){
+        /**
+         * Функция, возвращающая количество вхождений символов в слово. Используется счетчик индекса массива
+         */
+        text = text.toLowerCase();
+        int[] result = new int['я'-'a'+1];
+        System.out.println(Arrays.toString(result));
+        System.out.println(result.length);
+        System.out.println((int) 'а');
+        for(int i=0; i< text.length(); i++){
+            char ch = text.charAt(i); //символ по индексу
+            if (ch >= 'a' && ch<='я'){
+                result[ch-'а']++;
+            }
+        }
+        for (int i=0; i< 32;i++){
+            System.out.println((char)(i+'а')+" = "+result[i]);
+        }
+
+    }
+
+    public static void buildDictionaryWithMap(String text){
+        /**
+         * Более элегантный способ прошлого метода с использованием словаря
+         */
+        text=text.toLowerCase();
+        Map<Character, Integer> map = new HashMap<>();
+        for(int i=0;i<text.length();i++){
+            char ch = text.charAt(i);
+            if ((ch >='a' && ch<='я')|| ch == 'ё'){
+                map.compute(ch,((character, integer) -> integer == null ? 1 : integer+1));
+                //вставляем ключ ch в map  и значения integer
+            }
+        }
+        ArrayList<Map.Entry<Character, Integer>> entr = new ArrayList<>(map.entrySet()); // Map.Entry-поле с ключом-значением
+        entr.sort(((o1, o2) -> Character.compare(o1.getKey(), o2.getKey())));
+        for (Map.Entry<Character, Integer> enter: entr){
+            System.out.println(enter.getKey()+" = "+enter.getValue());
+        }
+    }
+    public static <K,V> Map <V,Collection<K>> revertMap(Map<? extends K, ? extends V> map){
+        /**
+         * Меняет ключ и значение местами
+         */
+        Map<V,Collection<K>> resultMap = new HashMap<>(); // создаем новый словарь
+
+        Set<K> keys = (Set<K>) map.keySet();
+        for (K key: keys){
+            V value = map.get(key); //получаем значение
+            resultMap.compute(value,(v,ks) -> { //вставляем как ключ
+                if(ks == null){
+                    ks = new HashSet<>();
+                }
+                ks.add(key);
+                return ks;
+            });
+        }
+        return resultMap;
+    }
+
+    }
+

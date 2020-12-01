1302d275a4287b9dbdc65302b66b57ab8ea22d9b
shycoldii <57770001+shycoldii@users.noreply.github.com>
2020-12-01 12:23:28 +0300

    семинар

---
+++ certification2/classwork/work01122020/lesson_spring/src/main/java/com/example/demo/repository/AnswerRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.Answer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AnswerRepository extends JpaRepository<Answer,Long> {
+    List<Answer> findByQuestionId(Long questionId);
+}
